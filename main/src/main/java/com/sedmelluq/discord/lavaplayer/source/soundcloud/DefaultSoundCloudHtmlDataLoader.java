451d728d24f7f41ba6db918bf9f27eed5a84501a
Presti <dxssucuk@hotmail.com>
2022-04-25 09:49:22 +0200

    Manually implemented fixes from: - https://github.com/sedmelluq/lavaplayer/pull/650/ - https://github.com/sedmelluq/lavaplayer/pull/631/

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/soundcloud/DefaultSoundCloudHtmlDataLoader.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/soundcloud/DefaultSoundCloudHtmlDataLoader.java
@@ -7,8 +7,10 @@ import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.tools.JsonBrowser;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpClientTools;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpInterface;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -19,31 +21,32 @@ import org.slf4j.LoggerFactory;
 import static com.sedmelluq.discord.lavaplayer.tools.FriendlyException.Severity.SUSPICIOUS;
 
 public class DefaultSoundCloudHtmlDataLoader implements SoundCloudHtmlDataLoader {
-  private static final Logger log = LoggerFactory.getLogger(DefaultSoundCloudHtmlDataLoader.class);
+    private static final Logger log = LoggerFactory.getLogger(DefaultSoundCloudHtmlDataLoader.class);
 
-  private static final TextRange[] JSON_RANGES = {
-      new TextRange("catch(e){}})},", ");</script>"),
-      new TextRange("){}})},", ");</script>")
-  };
+    private static final TextRange[] JSON_RANGES = {
+            new TextRange("window.__sc_hydration = ", ";</script>"),
+            new TextRange("catch(e){}})},", ");</script>"),
+            new TextRange("){}})},", ");</script>")
+    };
 
-  @Override
-  public JsonBrowser load(HttpInterface httpInterface, String url) throws IOException {
-    try (CloseableHttpResponse response = httpInterface.execute(new HttpGet(url))) {
-      if (response.getStatusLine().getStatusCode() == HttpStatus.SC_NOT_FOUND) {
-        return JsonBrowser.NULL_BROWSER;
-      }
+    @Override
+    public JsonBrowser load(HttpInterface httpInterface, String url) throws IOException {
+        try (CloseableHttpResponse response = httpInterface.execute(new HttpGet(url))) {
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_NOT_FOUND) {
+                return JsonBrowser.NULL_BROWSER;
+            }
 
-      HttpClientTools.assertSuccessWithContent(response, "video page response");
+            HttpClientTools.assertSuccessWithContent(response, "video page response");
 
-      String html = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-      String rootData = DataFormatTools.extractBetween(html, JSON_RANGES);
+            String html = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+            String rootData = DataFormatTools.extractBetween(html, JSON_RANGES);
 
-      if (rootData == null) {
-        throw new FriendlyException("This url does not appear to be a playable track.", SUSPICIOUS,
-            ExceptionTools.throwWithDebugInfo(log, null, "No track JSON found", "html", html));
-      }
+            if (rootData == null) {
+                throw new FriendlyException("This url does not appear to be a playable track.", SUSPICIOUS,
+                        ExceptionTools.throwWithDebugInfo(log, null, "No track JSON found", "html", html));
+            }
 
-      return JsonBrowser.parse(rootData);
+            return JsonBrowser.parse(rootData);
+        }
     }
-  }
 }
